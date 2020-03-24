b4fa3f55835bcf2cd01c04e995a4c3163fd18ee8
Bhargavamacha <mbsai29@gmail.com>
2020-03-24 08:08:52 +0530

    added lamda code

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,22 @@
+import boto3
+from botocore.client import Config
+import zipfile
+import io
+import mimetypes
+
+def lambda_handler(event, context):
+
+    s3 = boto3.resource("s3", config = Config(signature_version = 's3v4'))
+    portfolio_bucket = s3.Bucket('portfolio.bhargavamacha.info')
+    build_bucket = s3.Bucket("portfoliobuild.bhargavamacha.info")
+    
+    portfolio_zip = io.BytesIO()
+
+    build_bucket.download_fileobj('portfolio.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs = {'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
+    return 'lambda function executed!:)'
