d3bbb85abf449f76af834db1c5407ec2d4cea4d6
Bhargavamacha <mbsai29@gmail.com>
2020-03-24 11:58:01 +0530

    react

---
+++ js/example-work.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ExampleWork extends React.Component{
+    render(){
+        return(
+            <section>It works!</section>
+        )
+    }
+}
+export default ExampleWork
